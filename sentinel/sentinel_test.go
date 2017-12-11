bbe626212c006a8def2c4e526496ca7a3087f4fe
Kevin Wan <kevin@liao.com>
2017-12-11 09:59:35 +0800

    add cluster debug code

--- sentinel/sentinel_test.go
+++ sentinel/sentinel_test.go
@@ -6,7 +6,7 @@ import (
 	. "testing"
 	"time"
 
-	"github.com/mediocregopher/radix.v2/redis"
+	"github.com/kevwan/radix.v2/redis"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
